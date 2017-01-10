6727edebfca6d97dfae66efa9447369e0d5bfb43
vietsaint <anthonyvanluc@gmail.com>
2017-01-10 02:18:23 -0500

    Readme & Separated Info and App
    Added Readme containing notes to do and consider
    Made a separate main.html to handle app htmls
    Implemented quickstart google authentication

---
+++ public/js/alg.js
@@ -0,0 +1,4 @@
+/**
+ * Created by anthonyluc on
+ * Adapted a weighted bipartite matching algortithm
+ */
---
+++ public/js/main.js
@@ -0,0 +1,4 @@
+/**
+ * Created by anthonyluc on 1/9/17.
+ * Has main.html function calls
+ */
